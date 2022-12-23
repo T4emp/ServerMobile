06038db411c3cde6efd247d2fd5d0d3e65b0cebf
Администратор <@WIN-QH8F092I0OT>
2022-12-23 08:43:10 +0300

    Добавьте файлы проекта.

---
+++ ServerApp/Controllers/BookController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServerApp.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ServerApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BookController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+
+        public BookController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        [HttpGet("[action]")]
+        public IList<Book> GetList()
+        {
+            return _dataContext.Books.ToArray();
+        }
+
+        [HttpPost("[action]")]
+        public void Add(Book book)
+        {
+            _dataContext.Books.Add(book);
+
+            _dataContext.SaveChanges();
+        }
+
+        [HttpPost("[action]")]
+        public void Update(Book book)
+        {
+            _dataContext.Update(book);
+
+            _dataContext.SaveChanges();
+        }
+
+        [HttpPost("[action]")]
+        public void Delete(int id)
+        {
+            _dataContext.Books.RemoveRange(_dataContext.Books.Where(x => x.Id == id));
+
+            _dataContext.SaveChanges();
+        }
+    }
+}
---
+++ ServerApp/Models/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ServerApp.Models
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+
+        public DataContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Book;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+    }
+}
